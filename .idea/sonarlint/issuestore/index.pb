a9293a5820fb32c638815e10538d7dd02344f9b7
Freyja Ariadne <freyja.adne@gmail.com>
2023-05-17 02:01:18 -0500

    FirstCommit
    Initial UI created.
    No features currently implemented.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+M
+src/main/resources/apple.fxml,a\9\a9cad4e263af3346f50ee0f37e897595f70bd77d
+R
+"src/main/java/com/freyja/Main.java,0\e\0ec205959251f2cf98c987f795bdeee28f0bf6e0
+L
+src/main/resources/scene.css,f\e\fe4df0a60caa5ef2fb0b1509858017222686abc8
---
+++ src/main/java/com/freyja/Main.java
@@ -0,0 +1,26 @@
+package com.freyja;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/apple.fxml"));
+        Parent content = loader.load();
+        Scene scene = new Scene(content, 1280, 720);
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
